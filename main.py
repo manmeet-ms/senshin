799ede713697c030c6b348d2aa3e8d7eb3deba2a
Manmeet Singh <manmeets.zsh@gmail.com>
2023-08-29 23:41:51 +0530

    main push

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ import discord
 from  random import randint
 from requests import get
 import asyncio
-from dotenv import load_dotenv
-load_dotenv()
-DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
-# DISCORD_TOKEN = os.environ['DISCORD_TOKEN']
+# from dotenv import load_dotenv
+# load_dotenv()
+# DISCORD_TOKEN = os.getenv('DISCORD_TOKEN')
+DISCORD_TOKEN = os.environ['DISCORD_TOKEN']
 
 # quotes =["You will Gap in your comfort zone!", "Are you using Pomodoros?", "**Eat the Frog 🐸**\nConquer the struggling tasks like a pro with the Eat the Frog strategy.", "**20 second rule; Ikk vaari**\nOne step forward - Ikk vaari shuru kar k ta'an dekh technique (for 20 sec)", "**3 seconds rule**\nDefy procrastination by moving into action within 3 seconds. ", "**Move before you are motivated**\nMove before you're motivated (Count down to 3 sec Rule and START!)", "**Sugoi! That was just your 40%**\nPush past the 40% potential barrier. Your brain got governors push that to 100% performance", "**Procrastination steals your time; seize it back with dominance.**\nProcrastination is the thief of time, your enemy. (Dominate it, before it takes charge on yourself)", "**Dream ↑  Efforts ↑ ( D ∝ E)**\nAs your dreams rise, let your efforts soar.", "**You must ACT!**\n Indecision is the enemy; decisive action is the key.", "**Put ENERGY, Get RESULTS**\nInvest your energy, harvest incredible results.", "**Cut fkin' distractions**\nAchievement demands focus; eliminate distractions ruthlessly.", "The harder the battle, the sweeter the victory.", "FEAR is temporary, REGRET is permanent"]
 # for i in range(0, len(quotes)):
