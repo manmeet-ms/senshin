e7d38d319d682ed882dddf816bada9a0b0b5aba4
Manmeet Singh <manmeets.zsh@gmail.com>
2023-08-31 23:22:42 +0530

    ui color changes

--- script.js
+++ script.js
@@ -28,7 +28,7 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 async function Pomodoro(quoString) {
-    sendNotif(quoString)
+    // sendNotif(quoString)
     console.log(quoString)
     await sleep(5000);
 }
