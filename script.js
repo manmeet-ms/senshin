f4b388df605a27d8e88b11f29cac699470b6dead
Manmeet Singh <manmeets.zsh@gmail.com>
2023-09-02 23:05:33 +0530

    advice api added

--- script.js
+++ script.js
@@ -1,10 +1,27 @@
-console.log("Script start");
 let TimeModule = new Date();
 
 let Time = `${TimeModule.getHours()}:${TimeModule.getMinutes()}:${TimeModule.getSeconds()}`;
 Time = "18:00:00";
 console.log(Time)
-const quotes = ["Trying then fail is better than failing to try", "Get comfortable by being uncomfortable", "You will Gap in your comfort zone!", "Eat the Frog 🐸\nConquer the struggling tasks like a pro with the Eat the Frog strategy.", "20 second rule; Ikk vaari\nOne step forward - Ikk vaari shuru kar k ta'an dekh technique (for 20 sec)", "3 seconds rule\nDefy procrastination by moving into action within 3 seconds.", "Move before you are motivated\nMove before you're motivated (Count down to 3 sec Rule and START!)", "Sugoi! That was just your 40%\nPush past the 40% potential barrier. Your brain got governors push that to 100% performance", "Procrastination steals your Time; seize it back with dominance.\nProcrastination is the thief of Time, your enemy. (Dominate it, before it takes charge on yourself)", "Dream ↑  Efforts ↑ ( D ∝ E)\nAs your dreams rise, let your efforts soar.", "You must ACT!\n Indecision is the enemy; decisive action is the key.", "Put ENERGY, Get RESULTS\nInvest your energy, harvest incredible results.", "Cut fkin' distractions\nAchievement demands focus; eliminate distractions ruthlessly.", "The harder the battle, the sweeter the victory.", "FEAR is temporary, REGRET is permanent",
+const quotes = [
+"You will gap in your comfort zone!", 
+"🤞 Trying then fail is better than failing to try", 
+"🐸 Eat the Frog Conquer the struggling tasks like a pro with the Eat the Frog strategy.", 
+"⏲️ 20 second rule; Ikk vaari shuru kar k ta'an dekh technique (for 20 sec)", 
+"⏲️ 3, 2, 1 rule Kill procrastination by moving into action within 3 seconds.", 
+"🚀 Move before you crave for any motivation", 
+"0% Motivation 100% Discipline", 
+"Sugoi! That was just your 40% Push past the 40% potential barrier. Your brain got governors push that to 100% performance", 
+"Procrastination steals your Time; seize it back with dominance. Procrastination is the thief of Time, your enemy. (Dominate it, before it takes charge on yourself)", 
+"☘️ Dream ↑  Efforts ↑ ( D ∝ E) As your dreams rise, let your efforts soar.", 
+"You must ACT!  Indecision is the enemy; decisive action is the key.", 
+"⚡ Put ENERGY, Get RESULTS Invest your energy, harvest incredible results.", 
+"✂️ Cut fkin' distractions Achievement demands focus; eliminate distractions ruthlessly.", 
+"The harder the battle, the sweeter the victory.", 
+"FEAR is temporary, REGRET is permanent",
+"🎇 I submit to a higher power; they are acting through me.",
+"✨ Your worship directs the manifestation process.",
+"💖 As higher power says, as one worships me, that is how I manifest to them.",
 ];
 
 const sendNotif = (quoReceived) => {
@@ -27,7 +44,7 @@ const getRandomQuote = () => {
     return quotes[choice];
 };
 
-const pomodoroInterval = 0.1 * 60 * 1000; // in seconds
+const pomodoroInterval = 5.75 * 60 * 1000; // in minutes
 
 const updateQuoteAndNotify = () => {
     const quoString = getRandomQuote();
@@ -43,4 +60,17 @@ if (Notification.permission === "granted") {
      
     }
 }
-console.log("Script run");
+
+fetch('https://api.adviceslip.com/advice') 
+    .then(response => response.json())
+    .then(htmlContent => {
+        console.log("html", htmlContent);
+        obj2 = htmlContent
+        sendNotif(obj2["slip"]["advice"])
+    })
+    .catch(error => {
+        log=error
+        console.error('Error:', error);
+        sendNotif(error)
+        
+    });
